28d5e27d74440f6205d4af6166a9fb04d2d0cc6b
Thomas Folbrecht <tfolbrecht@gmail.com>
2019-03-05 18:58:40 -0500

    Added localstorage support

--- todo/src/index.js
+++ todo/src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { reducer } from './components/reducer';
+import { loadState, saveState } from './components/localStorage'
 import TodoList from './components/TodoList';
 import TodoForm from './components/TodoForm';
 import './index.css';
 
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const persistedState = loadState();
+const store = createStore(reducer, persistedState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+
+store.subscribe(() => {
+  saveState(store.getState());
+});
 
 function App() {
 return (
@@ -25,3 +32,10 @@ ReactDOM.render(
     </Provider>, 
     document.getElementById('root')
 );
+
+
+/*
+
+
+
+*/
